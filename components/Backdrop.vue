42d19e3f7e4a382a0bfaa2d02d70927fadd7a812
Tommaso De Marco <tommaso.demarco95@gmail.com>
2024-12-31 15:35:13 +0100

    Adds backdrop component

---
+++ components/Backdrop.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    @click="closeCart"
+    :class="[$style.backdrop, isCartOpen ? $style.open : $style.closed]"
+  ></div>
+</template>
+
+<script lang="ts" setup>
+const { isCartOpen, setIsCartOpen } = useCart();
+
+const closeCart = () => {
+  setIsCartOpen(false);
+};
+</script>
+
+<style module>
+.backdrop {
+  z-index: 1;
+  position: fixed;
+  background-color: transparent;
+  top: 0;
+  left: 0;
+  width: 0;
+  height: 0;
+  pointer-events: none;
+  transition: background-color 0.3s ease;
+}
+
+.backdrop.open {
+  background-color: rgba(0, 0, 0, 0.365);
+  width: 100%;
+  height: 100%;
+  pointer-events: auto;
+}
+</style>
