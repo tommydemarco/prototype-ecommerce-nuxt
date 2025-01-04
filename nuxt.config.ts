07e9b21aa8ec76e77de98af46a35f9a30a8b7668
Tommaso De Marco <tommaso.demarco95@gmail.com>
2025-01-04 15:42:35 +0100

    Adds engine configurations, adds start script, edits error page contents

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,4 +9,12 @@ export default defineNuxtConfig({
   runtimeConfig: {
     mongodbURI: process.env.MONGODB_URI,
   },
+  routeRules: {
+    "/": { prerender: true },
+    "/about": { prerender: true },
+    "/cookie-policy": { prerender: true },
+    "/frequently-asked-questions": { prerender: true },
+    "/privacy-policy": { prerender: true },
+    "/terms-and-conditions": { prerender: true },
+  },
 });
